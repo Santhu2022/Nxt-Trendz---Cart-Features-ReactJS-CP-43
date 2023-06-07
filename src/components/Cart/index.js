8706b6381f399770f9b99843a1529718d2b5a1fd
Santhu2022 <mogilipakasantosh@gmail.com>
2023-06-07 05:52:55 +0000

    cart features

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -3,6 +3,7 @@ import CartListView from '../CartListView'
 
 import CartContext from '../../context/CartContext'
 import EmptyCartView from '../EmptyCartView'
+import CartSummary from '../CartSummary'
 
 import './index.css'
 
@@ -12,14 +13,6 @@ const Cart = () => (
       const {cartList, removeAllCartItems} = value
       const cartLength = cartList.length
       const showEmptyView = cartLength === 0
-
-      const getOrderTotal = () => {
-        let total = 0
-        cartList.forEach(eachProduct => {
-          total += eachProduct.price * eachProduct.quantity
-        })
-        return total
-      }
 
       return (
         <>
@@ -40,17 +33,7 @@ const Cart = () => (
                   </button>
                 </div>
                 <CartListView />
-                <div className="cart-summary">
-                  <div>
-                    <h1>
-                      Order Total: <span>Rs {getOrderTotal()}/-</span>
-                    </h1>
-                    <p>
-                      {cartLength} {cartLength > 1 ? 'Items' : 'Item'} in cart
-                    </p>
-                  </div>
-                  <button type="button">Checkout</button>
-                </div>
+                <CartSummary />
               </div>
             )}
           </div>
